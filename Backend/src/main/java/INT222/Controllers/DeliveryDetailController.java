f8c7a1c0b72a4a6acc7fb0d57011712532afe07b
Traitawat Jitchana <traitawat.25957@mail.kmutt.ac.th>
2021-09-20 17:08:22 +0700

    Update propperty file

--- Backend/src/main/java/INT222/Controllers/DeliveryDetailController.java
+++ Backend/src/main/java/INT222/Controllers/DeliveryDetailController.java
@@ -1,4 +1,37 @@
 package INT222.Controllers;
 
+import INT222.Models.DeliveryDetails;
+import INT222.Repositories.DeliveryDetailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/delivery")
+@CrossOrigin(origins = "*")
 public class DeliveryDetailController {
+
+    @Autowired
+    private DeliveryDetailRepository deliveryDetailRepository;
+
+
+    @GetMapping("/list")
+    public List<DeliveryDetails> getReview() {
+        return deliveryDetailRepository.findAll();
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteById(@PathVariable(value = "id") long id) {
+        deliveryDetailRepository.deleteById(id);
+    }
+
+    @PutMapping("/update")
+    public void editReview(@RequestBody DeliveryDetails deliveryDetails) {
+        deliveryDetailRepository.save(deliveryDetails);
+    }
+
+    @PostMapping("/add")
+    public void addReview(@RequestBody DeliveryDetails deliveryDetails) {
+        deliveryDetailRepository.save(deliveryDetails);
+    }
 }
