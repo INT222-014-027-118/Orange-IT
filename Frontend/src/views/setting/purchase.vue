dac461d2e9e57b92a75a44974dc0859e2a677943
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-10-09 17:11:52 +0700

    [fix] purchase

--- Frontend/src/views/setting/purchase.vue
+++ Frontend/src/views/setting/purchase.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-white">
-        <dv class="flex flex-row justify-around">
+        <div class="flex flex-row justify-around">
             <button class="text-gray-600 text-xs md:text-base px-2 md:py-4 md:px-6 block hover:text-primary focus:outline-none text-primary border-b-2 font-medium border-primary">
                 all</button
             ><button class="text-gray-600 text-xs md:text-base px-2 md:py-4 md:px-6 block hover:text-primary focus:outline-none">
@@ -10,7 +10,7 @@
             ><button class="text-gray-600 text-xs md:text-base px-2 md:py-4 md:px-6 block hover:text-primary focus:outline-none">
                 done
             </button>
-        </dv>
+        </div>
         <p>purchase</p>
         <p>purchase</p>
         <p>purchase</p>
