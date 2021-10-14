4c07dcc4db17c38440071d5070ff1a4b6fceeeb1
Traitawat Jitchana <traitawat.25957@mail.kmutt.ac.th>
2021-10-15 01:43:39 +0700

    Update Users model

--- Backend/src/main/java/INT222/Models/Users.java
+++ Backend/src/main/java/INT222/Models/Users.java
@@ -34,10 +34,10 @@ public class Users {
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "user_roles",
             joinColumns = {
-                    @JoinColumn(name = "users_id")
+                    @JoinColumn(name = "user_id")
             },
             inverseJoinColumns = {
-                    @JoinColumn(name = "roles_name")
+                    @JoinColumn(name = "role_name")
             }
     )
     private Set<Roles> role;
