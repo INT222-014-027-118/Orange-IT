a9bd3882e5152454e4edf0ed8493c968bdc009d8
Top1568 <apisit.top@mail.kmutt.ac.th>
2021-10-15 00:30:01 +0700

    [Update]Frontend Login and state management and Database add color in cart item

--- Frontend/src/store/modules/authentication.js
+++ Frontend/src/store/modules/authentication.js
@@ -1,37 +1,23 @@
-import axios from "axios"
+
 
 const state = {
-    userinfo: {},
-    jwt: ''
+    userinfo: {}
 }
 
-const getters = {}
+const getters = {
+    userinfo: state => state.userinfo,
+}
 
 const actions = {
-    async login({commit},data){
-        axios
-            .post(`${process.env.VUE_APP_API}/authenticate`,data)
-            .then(res => {
-                console.log(data)
-                console.log(res)
-                let userinfo = res.data
-                commit('SET_USERINFO', userinfo)
-                commit('SET_JWT', userinfo.jwtToken)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
+
+
 }
 
 
 const mutations = {
-    SET_USERINFO(state, payload) {
-        state.userinfo = payload
-    },
-    SET_JWT(state, payload) {
-        state.jwt = payload
-    },
+    SET_USERINFO(state, data) {
+        state.userinfo = data
+    }
 }
 
 export default {
