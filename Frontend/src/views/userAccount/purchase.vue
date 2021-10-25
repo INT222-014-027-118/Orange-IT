0fe2c3438bef42dd75eaacaa3c911f19960d17ab
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-10-25 23:07:57 +0700

    [Update] purchase

--- Frontend/src/views/userAccount/purchase.vue
+++ Frontend/src/views/userAccount/purchase.vue
@@ -3,31 +3,34 @@
         <h1 class="text-3xl font-semibold p-3 capitalize">purchase</h1>
         <hr />
         <div class="flex flex-row justify-around">
-            <button class=" text-xs md:text-base px-2 md:py-4 md:px-6 block hover:text-primary focus:outline-none text-primary border-b-2 font-medium border-primary">
-                all</button
-            ><button class="text-gray-600 text-xs md:text-base px-2 md:py-4 md:px-6 block hover:text-primary focus:outline-none">
-                to be delivered</button
-            ><button class="text-gray-600 text-xs md:text-base px-2 md:py-4 md:px-6 block hover:text-primary focus:outline-none">
-                to be receive</button
-            ><button class="text-gray-600 text-xs md:text-base px-2 md:py-4 md:px-6 block hover:text-primary focus:outline-none">
-                done
+            <button
+                v-for="tab in tabs"
+                :key="tab"
+                @click="selectTab = tab"
+                class="text-gray-600 text-xs md:text-base px-2 md:py-4 md:px-6 block hover:text-primary focus:outline-none "
+                :class="[selectTab.id == tab.id ? 'text-primary border-b-2 font-medium border-primary' : '']"
+            >
+                {{ tab.title }}
             </button>
         </div>
-        <p>purchase</p>
-        <p>purchase</p>
-        <p>purchase</p>
-        <p>purchase</p>
-        <p>purchase</p>
-        <p>purchase</p>
-        <p>purchase</p>
-        <p>purchase</p>
-        <p>purchase</p>
-        <p>purchase</p>
+        <div></div>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            tabs: [
+                { id: 1, title: "all" },
+                { id: 2, title: "to be delivered" },
+                { id: 3, title: "to be receive" },
+                { id: 4, title: "done" },
+            ],
+            selectTab: { id: 1, title: "all" },
+        };
+    },
+};
 </script>
 
 <style></style>
