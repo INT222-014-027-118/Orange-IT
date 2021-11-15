746693ef60f80842c5a759e2c1571a4fb0026870
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-11-15 23:17:37 +0700

    [fix] search

--- Frontend/src/components/Search.vue
+++ Frontend/src/components/Search.vue
@@ -36,7 +36,7 @@
                         :class="[resultSeachName.length == 0 && index == 0 ? 'bg-primary text-white hover:bg-primaryfocus' : 'hover:bg-secondary hover:text-white text-black dark:text-gray-50']"
                         @click="
                             showBoxResult = false;
-                            $router.push({ name: 'resultProducts', params: { categoryName: cat.category } });
+                            $router.push({ name: 'resultProducts', params: { currentCategoryName: cat.category } });
                         "
                         @blur="showBoxResult = false"
                     >
