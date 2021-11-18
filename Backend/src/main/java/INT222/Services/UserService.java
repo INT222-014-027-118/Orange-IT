e830a33ce0add9a1dc20ed9196a9bc01789edf7d
Traitawat Jitchana <traitawat.25957@mail.kmutt.ac.th>
2021-11-18 17:46:30 +0700

    Update PreAuthorize to Controller V1.2

--- Backend/src/main/java/INT222/Services/UserService.java
+++ Backend/src/main/java/INT222/Services/UserService.java
@@ -32,12 +32,12 @@ public class UserService {
            if(userRepository.findTopByOrderByIdDesc()== null){
                adminUser.setId(1);
                adminUser.setUsername("admin123");
-               adminUser.setPassword(getEncodedPassword("admin@pass"));
+               adminUser.setPassword(getEncodedPassword("orangePassword"));
                this.registerNewAdmin(adminUser);
            }else
                adminUser.setId(userRepository.findTopByOrderByIdDesc().getId()+1);
            adminUser.setUsername("admin123");
-           adminUser.setPassword(getEncodedPassword("admin@pass"));
+           adminUser.setPassword(getEncodedPassword("orangePassword"));
            this.registerNewAdmin(adminUser);
        }
 
