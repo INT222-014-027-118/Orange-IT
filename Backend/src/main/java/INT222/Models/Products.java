a3c7bbd2a2af1e841903c87a7d4d7e5f0b3e2e3e
Traitawat Jitchana <traitawat.25957@mail.kmutt.ac.th>
2021-11-25 23:18:16 +0700

    Update Edit Product

--- Backend/src/main/java/INT222/Models/Products.java
+++ Backend/src/main/java/INT222/Models/Products.java
@@ -59,7 +59,7 @@ public class Products {
     @JoinTable(name = "Products_has_categories", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private List<Categories> categories = new ArrayList<>();
 
-    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
     @JoinColumn(name = "product_id",insertable = true, updatable = true)
     private List<ProductsHasAttributes> productsHasAttributes;
 
