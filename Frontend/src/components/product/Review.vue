365e62c713ebc22937de12b675f3285ea54caf83
Top1568 <apisit.top@mail.kmutt.ac.th>
2021-12-09 01:03:22 +0700

    Update Feature Review for user

--- Frontend/src/components/product/Review.vue
+++ Frontend/src/components/product/Review.vue
@@ -17,88 +17,7 @@
                 <p class="sr-only">{{ reviews.average }} out of 5 stars</p>
                 <a href="reviews.href" class="ml-3 text-sm font-medium text-primary hover:text-secondary">{{ reviews.totalCount }} reviews</a>
             </div>
-            <div class="mt-3">
-                <div class="flex items-center mt-1">
-                    <div class="w-1/5 text-center text-primary tracking-tighter">
-                        <span>5 star</span>
-                    </div>
-                    <div class="w-3/5">
-                        <div class="bg-gray-300 w-full rounded-lg h-2">
-                            <!-------------------------------- revie ---------------------------------->
-                            <div class="bg-secondary rounded-lg h-2" :style="{ width: '51%' }"></div>
-                        </div>
-                    </div>
-                    <div class="w-1/5 text-center text-gray-700 dark:text-gray-400 pl-3">
-                        <!-----------------%%%%% percent %%%%---------------->
-                        <span class="text-sm">51%</span>
-                    </div>
-                </div>
-                <!-- first -->
-                <div class="flex items-center mt-1">
-                    <div class="w-1/5 text-center text-primary tracking-tighter">
-                        <span>4 star</span>
-                    </div>
-                    <div class="w-3/5">
-                        <div class="bg-gray-300 w-full rounded-lg h-2">
-                            <!-------------------------------- revie ---------------------------------->
-                            <div class="bg-secondary rounded-lg h-2" :style="{ width: '17%' }"></div>
-                        </div>
-                    </div>
-                    <div class="w-1/5 text-center text-gray-700 dark:text-gray-400 pl-3">
-                        <!-----------------%%%%% percent %%%%---------------->
-                        <span class="text-sm">17%</span>
-                    </div>
-                </div>
-                <!-- second -->
-                <div class="flex items-center mt-1">
-                    <div class="w-1/5 text-center text-primary tracking-tighter">
-                        <span>3 star</span>
-                    </div>
-                    <div class="w-3/5">
-                        <div class="bg-gray-300 w-full rounded-lg h-2">
-                            <!-------------------------------- revie ---------------------------------->
-                            <div class=" bg-secondary rounded-lg h-2" :style="{ width: '19%' }"></div>
-                        </div>
-                    </div>
-                    <div class="w-1/5 text-center text-gray-700 dark:text-gray-400 pl-3">
-                        <!-----------------%%%%% percent %%%%---------------->
-                        <span class="text-sm">19%</span>
-                    </div>
-                </div>
-                <!-- thierd -->
-                <div class="flex items-center mt-1">
-                    <div class=" w-1/5 text-center text-primary tracking-tighter">
-                        <span>2 star</span>
-                    </div>
-                    <div class="w-3/5">
-                        <div class="bg-gray-300 w-full rounded-lg h-2">
-                            <!-------------------------------- revie ---------------------------------->
-                            <div class=" bg-secondary rounded-lg h-2" :style="{ width: '8%' }"></div>
-                        </div>
-                    </div>
-                    <div class="w-1/5 text-center text-gray-700 dark:text-gray-400 pl-3">
-                        <!-----------------%%%%% percent %%%%---------------->
-                        <span class="text-sm">8%</span>
-                    </div>
-                </div>
-                <!-- 4th -->
-                <div class="flex items-center mt-1">
-                    <div class="w-1/5 text-center text-primary tracking-tighter">
-                        <span>1 star</span>
-                    </div>
-                    <div class="w-3/5">
-                        <div class="bg-gray-300 w-full rounded-lg h-2">
-                            <!-------------------------------- revie ---------------------------------->
-                            <div class=" bg-secondary rounded-lg h-2" :style="{ width: '5%' }"></div>
-                        </div>
-                    </div>
-                    <div class="w-1/5 text-center text-gray-700 dark:text-gray-400 pl-3">
-                        <!-----------------%%%%% percent %%%%---------------->
-                        <span class="text-sm">5%</span>
-                    </div>
-                </div>
-                <!-- 5th -->
-            </div>
+           
         </div>
         <!-- <div class="w-full px-4">
             <h3 class="font-medium tracking-tight">Review this item</h3>
@@ -119,6 +38,9 @@ export default {
                 average: 4,
             },
         };
+    },
+    props:{
+        reviews:Array
     },
     created() {
     },
